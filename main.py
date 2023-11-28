deaf4dca2a161957bf3e3f5c083783a4404c378a
Guilherme Neves da Silva <guilhermenevesdasilva321@gmail.com>
2023-11-28 20:59:07 -0300

    fix: mudanças para mongo para que funcione

--- main.py
+++ main.py
@@ -1,103 +1,76 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator
-from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.postgresql import UUID 
-from sqlalchemy.ext.declarative import declarative_base
-from typing import List
-from datetime import date
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel, constr
+from pymongo import MongoClient
 import uuid
 
+client = MongoClient("mongodb://user_root:pass_root@db:27017")
+db = client["Projetinho"]["pessoas"]
 
-app = FastAPI()
-DB_URL = 'postgresql://user_root:pass_root@db:5433/postgres'  
+app = FastAPI(title='Projeto Nike')
 
-engine = create_engine(DB_URL)
+class PessoaCreate(BaseModel):
+    apelido: constr(max_length=32)
+    nome: constr(max_length=100)
+    nascimento: str
+    stack: list[str] = None
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-class Pessoa(BaseModel):
+class PessoaResponse(BaseModel):
+    id: str
     apelido: str
     nome: str
-    nascimento: date
-    stack: List[str] = []
+    nascimento: str
+    stack: list[str] = None
 
-    @validator('stack', each_item=True)
-    def validate_stack_item_length(cls, stack_item):
-        if len(stack_item) > 32:
-            raise ValueError('Stack item length should not exceed 32 characters')
-        return stack_item
+@app.post('/pessoas', response_model=PessoaResponse, status_code=201)
+def create_pessoa(pessoa: PessoaCreate):
+
+    new_pessoa = {
+        'id': str(uuid.uuid4()),
+        'apelido': pessoa.apelido,
+        'nome': pessoa.nome,
+        'nascimento': pessoa.nascimento,
+        'stack': pessoa.stack
+    }
     
-    @validator('nome')
-    def validate_nome_length(cls, nome):
-        if not nome:
-            raise ValueError('O campo "nome" é obrigatório')
-        if len(nome) > 100:
-            raise ValueError('O campo "nome" deve ter no máximo 100 caracteres')
-        return nome
-    
-    @validator('apelido')
-    def validate_apelido(cls, apelido):
-        if not apelido:
-            raise ValueError('O campo "apelido" é obrigatório')
-        if len(apelido) > 32:
-            raise ValueError('O campo "apelido" deve ter no máximo 32 caracteres')
-        return apelido
-    
-    @validator('nascimento')
-    def validate_date_format(cls, nascimento):
-        try:
-            # Tenta converter a string em um objeto de data no formato "AAAA-MM-DD"
-            date.fromisoformat(nascimento)
-        except ValueError:
-            raise ValueError('O campo "nascimento" deve estar no formato "AAAA-MM-DD"')
+    db.insert_one(new_pessoa)
 
-        return nascimento
-    
-Base = declarative_base()
+    return new_pessoa
 
-class PessoaDB(Base):
-    __tablename__ = "pessoas"
-    id = Column(String, primary_key=True, index=True, default=str(uuid.uuid4))  # Use String para UUID
-    apelido = Column(String, index=True)
-    nome = Column(String)
-    nascimento = Column(Date)
-    stack = Column(String)
+@app.get('/pessoas/{id}', response_model=PessoaResponse)
+async def find_by_id(id: str):
+    pessoa = db.find_one({"id": id})
+    if pessoa:
+        return pessoa
+    raise HTTPException(status_code=404, detail="Pessoa não encontrada")
 
-# Crie as tabelas no banco de dados
-Base.metadata.create_all(bind=engine)
+@app.get('/pessoas', response_model=list[PessoaResponse])
+def find_by_term(term: str = Query(..., min_length=1)):
+    results = list(db.find({
+        "$or": [
+            {"apelido": {"$regex": term, "$options": "i"}},
+            {"nome": {"$regex": term, "$options": "i"}},
+            {"stack": {"$elemMatch": {"$regex": term, "$options": "i"}}}
+        ]
+    }))
+    return results
 
-class RespostaPessoa(BaseModel):
-    message: str
-    pessoa: Pessoa
+@app.get("/contagem-pessoas")
+def count_pessoas():
+    count = db.count_documents({})
+    return count
 
-# POST endpoint to create a Pessoa
-@app.post("/pessoas", response_model=RespostaPessoa, status_code=201)
-def criar_pessoa(pessoa:Pessoa):
-    try:
-        if any(len(item) > 32 for item in pessoa.stack):
-            raise HTTPException(status_code=442, detail="Unprocessable Entity/Content")
-        db_pessoa = PessoaDB(**pessoa.dict())
-        with SessionLocal() as session:
-            session.add(db_pessoa)
-            session.commit()
-            session.refresh(db_pessoa)
-        location_url = f"/pessoas/{db_pessoa.id}"
-        response_headers = {"Location": location_url}
+@app.get("/teste_db")
+def teste_db():
+    ping = client.server_info()
+    return {"ping": ping}
 
-        response_body = {"message":"Pessoa criada com sucesso", "pessoa": db_pessoa}
-        return response_body, response_headers 
-    except:
-        raise HTTPException(status_code=442, detail="Unprocessable Entity/Content")
-
-@app.get("/pessoas/{pessoa_id}", response_model=RespostaPessoa)
-def pegar_pessoa(pessoa_id: uuid.UUID):
-    try:
-        with SessionLocal() as session:
-            db_pessoa = session.query(PessoaDB).filter(PessoaDB.id==pessoa_id).first()
-            if db_pessoa is None:
-                raise HTTPException(status_code=404, detail="Pessoa not found")
-            return db_pessoa
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error")
+@app.get('/pessoas')
+def search_people_by_term(t: str = Query(..., min_length=1, alias='term')):
+    results = list(db.find({
+        "$or": [
+            {"apelido": {"$regex": t, "$options": "i"}},
+            {"nome": {"$regex": t, "$options": "i"}},
+            {"stack": {"$elemMatch": {"$regex": t, "$options": "i"}}}
+        ]
+    }))
+    return results
